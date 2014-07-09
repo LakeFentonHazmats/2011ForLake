1763e2fb4c74a9a22fc08cfbdb75ffd40a87892e
Nicholas J. Knoch <nicster34@gmail.com>
2014-07-09 14:45:16 -0400

    Blahhh

---
+++ src/edu/wpi/first/wpilibj/templates/subsystems/ExtendArm.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates.subsystems;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.templates.commands.Extend;
+import edu.wpi.first.wpilibj.templates.RobotMap;
+
+
+/**
+ *
+ * @author Nicster34
+ */
+public class ExtendArm extends Subsystem {
+    Victor extendArmed = new Victor(RobotMap.rightMotor);
+    public ExtendArm(){
+        
+    }
+            
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        setDefaultCommand(new Extend());
+    }
+    public void upDown(double move) {
+        extendArmed.set(move);
+    }
+}
+
---
+++ src/edu/wpi/first/wpilibj/templates/subsystems/UpDownArm.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.templates.commands.Raise;
+import edu.wpi.first.wpilibj.templates.RobotMap;
+/**
+ *
+ * @author Nicster34
+ */
+public class UpDownArm extends Subsystem {
+   Victor upDownArmed = new Victor(RobotMap.leftMotor);
+    public UpDownArm(){
+        
+    }
+            
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        setDefaultCommand(new Raise());
+    }
+    public void upDown(double moveup) {
+        upDownArmed.set(moveup);
+    }
+}
