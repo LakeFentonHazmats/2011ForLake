9d25f4c8d4b6f3d45e338b3fb110212b6ee530c5
Nic Knoch <nicster34@gmail.com>
2014-07-26 14:20:45 -0400

    Drive Train

--- src/edu/wpi/first/wpilibj/templates/subsystems/DriveTrain.java
+++ src/edu/wpi/first/wpilibj/templates/subsystems/DriveTrain.java
@@ -6,10 +6,26 @@
 
 package edu.wpi.first.wpilibj.templates.subsystems;
 
+import edu.wpi.first.wpilibj.templates.RobotMap;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.templates.commands.ExampleCommand;
+
 /**
  *
  * @author Nicster34
  */
-public class DriveTrain {
+public class DriveTrain extends Subsystem{
+    RobotDrive robotDrive = new RobotDrive(RobotMap.leftFrontMotor, RobotMap.leftBackMotor, RobotMap.rightBackMotor, RobotMap.rightFrontMotor);
+    
+    public void initDefaultCommand() {
+        setDefaultCommand(new ExampleCommand());
+        
+        
+    }
+    public void mecanumDrive() {
+        robotDrive.mecanumDrive_Cartesian(RobotMap.leftBackMotor, RobotMap.rightBackMotor, RobotMap.rightFrontMotor, RobotMap.leftFrontMotor);
+    }
     
 }
+
